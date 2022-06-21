c4416dd976c4f1f9091ccffd313260e5837f45be
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-06-21 19:53:58 -0400

    update

--- src/main/java/com/example/learningStrings/practice/practiceString.java
+++ src/main/java/com/example/learningStrings/practice/practiceString.java
@@ -2,14 +2,19 @@ package com.example.learningStrings.practice;
 
 public class practiceString {
     public static void main(String[] args) {
+
         String str = "Learn String Handling.";
         String allUpper = "LEARN STRING HANDLING";
+
+
         boolean isTrue = false;
 
+        //charAt()
         char ch1 = str.charAt(0);
         char ch2 = str.charAt(1);
         char ch3 = str.charAt(4);
 
+        //equalsIgnoreCase()
         if(str.equalsIgnoreCase(allUpper)) {
             isTrue = true;
         }else {
@@ -23,6 +28,7 @@ public class practiceString {
         System.out.println("Are str and allUpper equal: " + isTrue);
 
         // 1) Using the string "Learn string handling": what index is the last letter in the string?
+        //length()
         System.out.println("The last index in the string str is: " + (str.length()-1));
 
         // 2) Using the same string as above, what character is located at the 10th index?
@@ -34,16 +40,26 @@ public class practiceString {
         // 4) Create a scenario that returns false if the character at the 0 index equals the 5 index
         System.out.println("Scenario created to return false if character at 0 index equals the 5 index: " + !(str.toCharArray()[0] == str.toCharArray()[5]));
 
-        //length()
         //equals()
-        //equalsIgnoreCase()
-        //charAt()
+        System.out.println("Is Str equal to allUpper: " + str.equals(allUpper));
+
         //contains()
+        System.out.println("Does str contain 'ing' in it?: " + str.contains("ing"));
+
         //substring()
+        System.out.println("The substring of str from index 4 to 9 is: " + str.substring(4, 9));
+
         //startsWith
+        System.out.println("does str starts with 'ch' at index 5: " + str.startsWith("ch", 5));
+
         //endsWith
+        System.out.println("does str ends with 'ing': " + str.endsWith("ing"));
+
         //toLowerCase()
+        System.out.println("Converting allUpper to lowercase: " + allUpper.toLowerCase());
+
         //toUpperCase()
+        System.out.println("Converting str to uppercase: " + str.toUpperCase());
     }
 }
 
